888b6663bbe6ad3df4bdcb4a4d3acd27f6de5055
Dandi M. Reza <dandimr16@gmail.com>
2022-02-15 11:18:18 +0700

    Server XMLRPC

--- Tugas1.py
+++ Tugas1.py
@@ -16,17 +16,17 @@ with SimpleXMLRPCServer(("127.0.0.1", 8008),
     
     class MyFuncs:
         def mul(self, x, y):
-            return x * y
+            return x * y##Return pembagian dari x dan y
     
     server.register_instance(MyFuncs())
 
     def minus_function(x, y):
-        return x - y
+        return x - y##Return pembagian dari x dan y
     
     server.register_instance(minus_function, 'minus')
 
     def divide_function(x, y):
-        return x / y
+        return x / y##Return pembagian dari x dan y
     
     server.register_instance(divide_function, 'div')
 
