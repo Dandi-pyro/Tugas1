794c00b98f8790774da08fd1a6adcb5e40f633fb
Dandi M. Reza <dandimr16@gmail.com>
2022-02-15 10:42:05 +0700

    Server XMLRPC

---
+++ Tugas1.py
@@ -0,0 +1,33 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+     rpc_paths = ('/RPC2',)
+
+with SimpleXMLRPCServer(("127.0.0.1", 8008),
+                        requestHandler=RequestHandler) as server:
+    server.register_introspection_functions()
+
+    server.register_function(pow)
+
+    def adder_function(x,y):
+        return x + y
+    
+    class MyFuncs:
+        def mul(self, x, y):
+            return x * y
+    
+    server.register_instance(MyFuncs())
+
+    def minus_function(x, y):
+        return x - y
+    
+    server.register_instance(minus_function, 'minus')
+
+    def divide_function(x, y):
+        return x / y
+    
+    server.register_instance(divide_function, 'div')
+
+    server.serve_forever()
